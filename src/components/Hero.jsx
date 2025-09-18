fe1d870f0c33fd27ef0a2602feda1f4e496edc54
wisdom michael <ijarwisdom@gmail.com>
2025-09-18 15:17:09 -0700

    img changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,13 @@
  * @license Apache-2.0
  */
 
-"use client"
-
 import { motion } from "framer-motion"
 import { useEffect, useState } from "react"
 
 // components
 import { ButtonPrimary, ButtonOutline } from "./Button"
 
-const words = ["Scalable", "Fast", "Dynamic", "Powerful", "Efficient", "Modern", "Innovative"]
+const words = ["Scalable", "Fast", "Dynamic", "Powerful", "Efficient","Modern","Robust", "Secure", "Sleek", "Intuitive"];
 
 const Hero = () => {
   const [index, setIndex] = useState(0)
@@ -26,9 +24,9 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="gradient pt-28 lg:pt-36"
+      className="gradient pt-24 lg:pt-36"
     >
-      <div className="container lg:grid lg:grid-cols-2 items-center lg:gap-10">
+      <div className="container lg:grid lg:grid-cols-2 grid-cols-1 items-center lg:gap-10">
 
         {/* LEFT SECTION (Reveals when in view) */}
         <motion.div
@@ -41,7 +39,7 @@ const Hero = () => {
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg overflow-hidden">
               <img
-                src="https://res.cloudinary.com/duwfbyhyq/image/upload/v1735987981/myself_ywogoy.jpg"
+                src="https://res.cloudinary.com/duwfbyhyq/image/upload/v1758232538/me_i2tq0n.jpg"
                 width={40}
                 height={40}
                 alt="wisdom michaels image"
@@ -57,7 +55,7 @@ const Hero = () => {
             </div>
           </div>
 
-          <h2 className="headline-1 text-[#a1a1aa]  max-w-[15ch] sm:mx-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
+          <h2 className="headline-1 text-[#a1a1aa]  max-w-[15ch] sm:mx-w-[20ch] lg:max-w-[15ch] mt-5 lg:mb-10">
             Building{" "}
             <motion.span
               key={index} // re-renders with new word
@@ -65,14 +63,30 @@ const Hero = () => {
               animate={{ y: 0, opacity: 1 }}
               exit={{ y: 30, opacity: 0 }}
               transition={{ duration: 0.9, ease: "easeOut" }}
-              className="inline-block !text-purple-600 min-w-[7ch]"
+              className="inline-block !text-purple-600 min-w-fit"
             >
               {words[index]}
-            </motion.span>{" "}
-            Modern Websites for the Future
+          </motion.span>{" "}
+           Websites for the Future
           </h2>
 
-          <div className="flex items-center gap-3">
+           <motion.div
+          initial={{ opacity: 0, x: 80 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
+          viewport={{ once: true, amount: 0.3 }}
+          className=" pb-5 pt-3 lg:hidden"
+        >
+          <figure className="w-full flex justify-center items-center rounded-[20px] shadow-purple-600/30 overflow-hidden shadow-2xl">
+            <img
+              src="https://res.cloudinary.com/duwfbyhyq/image/upload/v1758232538/me_i2tq0n.jpg"
+              alt="wisdom michaels futuristic image"
+              className="w-full flex justify-center items-center"
+            />
+          </figure>
+        </motion.div>
+
+          <div className="flex justify-start items-center gap-3 lg:gap-7 pt-2">
             <ButtonPrimary
               href={"https://flowcv.com/resume/86htsgsidk"}
               label="Download CV"
@@ -95,9 +109,9 @@ const Hero = () => {
           viewport={{ once: true, amount: 0.3 }}
           className="hidden lg:block"
         >
-          <figure className="w-[70%] ml-[85px] max-w-[480px] flex justify-center items-center bg-gradient-to-t from-purple-800 via-purple-600/40 to-65% rounded-[60px] overflow-hidden shadow-2xl">
+          <figure className="w-[90%] ml-[20px] flex justify-center items-center rounded-[30px] shadow-purple-600/30 overflow-hidden shadow-2xl">
             <img
-              src="https://res.cloudinary.com/duwfbyhyq/image/upload/v1736026929/result_1_2_1_xyzwpt.png"
+              src="https://res.cloudinary.com/duwfbyhyq/image/upload/v1758232538/me_i2tq0n.jpg"
               alt="wisdom michaels futuristic image"
               className="w-full flex justify-center items-center"
             />
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -38,7 +38,7 @@ const works = [
       projectLink: 'https://wisdommichaels.github.io/Realtime_Weather_App/'
     },
     {
-      imgSrc: 'https://res.cloudinary.com/duwfbyhyq/image/upload/v1736889145/Screenshot_2025-01-07_113433_ihnll1.png',
+      imgSrc: 'https://res.cloudinary.com/duwfbyhyq/image/upload/v1758233380/22_ncpwb2.jpg',
       title: 'My Personal Portfolio',
       tools: ['Web-design', 'Development'],
       projectLink: 'https://portfolio-rzig.vercel.app/'
